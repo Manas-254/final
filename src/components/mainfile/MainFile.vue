fd3179ee80e6e49c9c8524577c804f57da469bf2
Sathish G <sathish740g@gmail.com>
2021-10-19 17:50:00 +0530

    files added

--- src/components/mainfile/MainFile.vue
+++ src/components/mainfile/MainFile.vue
@@ -1,9 +1,10 @@
 <template>
-<h1>Home Page</h1>
-<h2>Users</h2>
+<h2>Shopping Cart</h2>
   <div class="container-main">
     <div class="box" v-for="(user, index) in apiData" :key="index">
-      <img :src="user.avatar" @click="getDetails(user),scrollToTop()" :userdetails="user" />
+      <img :src="user.image" @click="getDetails(user),scrollToTop()" :userdetails="user" />
+    <p class="info" v-html="user.title"></p>
+    <p class="info" style="font-weight: 700">Price:{{user.price}}</p>
     </div>
   </div>
 </template>
@@ -27,41 +28,52 @@ export default {
     scrollToTop() {
       window.scrollTo(0, 0);
     },
+    
   },
 
   async created() {
     console.log("main");
-    const response = await fetch("https://reqres.in/api/users/");
+    const response = await fetch("https://fakestoreapi.com/products");
     const result = await response.json();
     console.log("result", result);
-    this.apiData = result.data;
+    this.apiData = result;
     console.log("apiData", this.apiData);
   },
 };
 </script>
 
-<style>
+<style scoped>
 * {
   box-sizing: border-box;
 }
 .container-main{
   display:grid;
-  grid-template-columns:repeat(6,1fr);
+  grid-template-columns:repeat(4,1fr);
   padding-left: 60px;
+ background-color: currentcolor;
+ 
 }
 .box {
   margin: 10px;
-  align-items: center;
+    align-items: center;
+    width: auto;
+    height: auto;
+    border: ridge;
+    background-color: white;
 }
 img {
-  border: 5px solid rgb(161, 216, 10);
-  width: 160px;
+  border: 5px solid  rgb(34 127 119);
+  width: 250px;
   margin: 40px auto;
-  border-radius: 50%;
   cursor: pointer;
 }
 h2{
   align-content: initial;
- 
+      font-size: 40px;
+    color: darkslategrey;
+}
+.info{
+  color: blue;
+    font-size: larger;
 }
 </style>
--- src/components/view/Details.vue
+++ src/components/view/Details.vue
@@ -1,20 +1,37 @@
 <template>
-  <button class="backbtn" @click="this.$router.back()">Back</button>
+  <!-- <button class="backbtn" @click="this.$router.back()">Back</button> -->
   <div class="container">
-    
     <h1>
-      <span> Profile User {{ individualDetails.id }}</span>
+      <span> Item {{ individualDetails.id }}</span>
     </h1>
     <div class="card">
-      <img :src="individualDetails.avatar" alt="images" />
+      <img :src="individualDetails.image" alt="images" />
       <div class="content">
         <p class="info" style="font-weight: 700">
-         Name: {{ individualDetails.first_name }} {{ individualDetails.last_name }}
+          Name: {{ individualDetails.title }}
         </p>
         <p class="info" style="font-weight: 600">
-          Email: {{ individualDetails.email }}
+          Price: {{ individualDetails.price }}
         </p>
-        
+        <p class="info" style="font-weight: 600">
+          Desc: {{ individualDetails.description }}
+        </p>
+        <p class="info" style="font-weight: 600">
+          Category: {{ individualDetails.category }}
+        </p>
+        <!-- <p class="info" style="font-weight: 600">
+          Rating: {{ individualDetails.rating }}
+        </p> -->
+        <!-- <button class="product_btn" @click="addToCart">Quick add</button> -->
+        <div class="butt">
+          <button
+            @click="removeFromCart(individualDetails.id)"
+            v-if="isInCart(individualDetails.id)"
+          >
+            remove from cart
+          </button>
+          <button @click="addToCart()" v-else>add to cart</button>
+        </div>
       </div>
     </div>
   </div>
@@ -26,31 +43,67 @@ export default {
   data() {
     return {
       individualDetails: "",
+      cart: [],
+      items: [],
     };
   },
   created() {
-    console.log("route.params", JSON.parse(this.$route.params.details));
+    var id1;
     this.individualDetails = this.$route.params.details
       ? JSON.parse(this.$route.params.details)
       : null;
-    console.log("user", this.individualDetails);
+    console.log("userdetails||", this.individualDetails);
+    id1 = this.individualDetails.id;
+    this.items = Object.freeze(this.individualDetails);
+    console.log("items||", this.items);
+    console.log("id||", id1);
   },
-  // props:{
-  //   userdetails:{
-  //     type:Array,
-  //     default:null
-  //   },
-  // }
+  methods: {
+    isInCart(itemId) {
+      if (!localStorage.getItem("cart")) {
+        localStorage.setItem("cart", JSON.stringify([]));
+      }
+      console.log(itemId);
+      const cartItem = this.cart.find(({ id }) => id == itemId);
+      return Boolean(cartItem);
+    },
+    addToCart() {
+      const item = this.items.id;
+      const itemname = this.items.title;
+      const itemimage = this.items.image;
+      const itemprice = this.items.price;
+      console.log(localStorage.getItem("cart"));
+      if (!localStorage.getItem("cart")) {
+        localStorage.setItem("cart", JSON.stringify([]));
+      }
+      const cartItems = JSON.parse(localStorage.getItem("cart"));
+      cartItems.push({
+        "id": item,
+        "name": itemname,
+        "image": itemimage,
+        "total":0, 
+        "price": itemprice, 
+        "quantity":0,
+      });
+      localStorage.setItem("cart", JSON.stringify(cartItems));
+      this.cart = JSON.parse(localStorage.getItem("cart"));
+      console.log("cart||", this.cart);
+    },
+    removeFromCart(itemId) {
+      const cartItems = JSON.parse(localStorage.getItem("cart"));
+      const index = cartItems.findIndex((id) => id === itemId);
+      cartItems.splice(index, 1);
+      localStorage.setItem("cart", JSON.stringify(cartItems));
+      this.cart = JSON.parse(localStorage.getItem("cart"));
+    },
+  },
 };
 </script>
 
 <style scoped>
 .container {
-  /* background-color: aquamarine; */
-  /* height: 600px; */
   width: 60%;
   margin: 0 auto;
-  
 }
 h1 {
   color: black;
@@ -61,30 +114,49 @@ span {
 }
 .card {
   margin: 30px auto;
-  border: 3px solid black;
-    display: inline-block;
-    border-radius: 10%;
-    width: 300px;
+  border: 5px solid black;
+  display: flex;
+  border-radius: 10px;
+  align-items: center;
+  text-align: left;
 }
 img {
-  margin: 10px auto;
-  /* width:100%;
-  height: 100%; */
+  margin: 20px auto;
+  margin-left: 20px;
+  border: outset;
+  height: 300px;
 }
 .content {
-  margin: 0 auto;
+  margin-left: 20px;
 }
 .info {
   margin: 5px 0;
+  color: navy;
+  font-stretch: semi-condensed;
+  padding: 5px;
 }
 .backbtn {
   border: none;
-  background-color:black;
+  background-color: black;
   padding: 10px;
   border-radius: 10px;
   color: white;
   cursor: pointer;
   margin: 20px 0;
-  float:left;
+  float: left;
+}
+.product_btn {
+  margin: 10px 0;
+  padding: 10px;
+  cursor: pointer;
+  font-size: 15px;
+  font-weight: bold;
+  background-color: rgb(85, 206, 15);
+  color: white;
+  border-radius: 20px;
+}
+.butt1 {
+  position: relative;
+  transform: translate(-280px, 450px);
 }
 </style>
